401a024a00e6f958f3c9969445f19b05a8ad1b8f
anaadx <anapaula.assisduarte.99@gmail.com>
2023-07-04 10:52:42 -0300

    feat: event img and buttons ok

--- src/scripts/community.js
+++ src/scripts/community.js
@@ -30,4 +30,22 @@ $(document).ready(function () {
       },
     },
   });
+
+  $(".owl-carousel").each(function () {
+    var owl = $(this);
+    owl.on("click", ".owl-item", function (event) {
+      var currentItem = $(this);
+      var activeItem = owl.find(".owl-item.active");
+
+      if (!currentItem.hasClass("active")) {
+        event.preventDefault();
+
+        if (currentItem.index() < activeItem.index()) {
+          owl.trigger("prev.owl.carousel", [300]);
+        } else {
+          owl.trigger("next.owl.carousel", [300]);
+        }
+      }
+    });
+  });
 });
